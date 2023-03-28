ec3cb8e846dc36b07dbc1587fa7a41cd6e277e33
Nomaan Ahmad <94966523+nomaannn@users.noreply.github.com>
2023-03-28 08:07:28 +0530

    created a submission

--- script.js
+++ script.js
@@ -8,9 +8,9 @@ for (let i = 0; i < arr.length-2; i++) {
 	for (let j = i+1; j < arr.length-1; j++) {
 		for (let k = j+1; k < arr.length; k++) {
 			if(arr[i]+arr[j]+arr[k]>=target){
-				 min = arr[i]+arr[j]+arr[k] - target;
-				if(ans<min){
-					ans = min;
+				 let minus = arr[i]+arr[j]+arr[k] - target;
+				if(minus>min){
+					min = minus;
 				}
 			}
 		}
@@ -18,7 +18,7 @@ for (let i = 0; i < arr.length-2; i++) {
 }
 
 
-return ans;
+return min;
 }
 
 module.exports = threeSum;
