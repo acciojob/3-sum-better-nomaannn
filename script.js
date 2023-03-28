ab146a253526f608bc0396b6f176c3936c8dbc8a
Nomaan Ahmad <94966523+nomaannn@users.noreply.github.com>
2023-03-28 08:05:33 +0530

    created a submission

--- script.js
+++ script.js
@@ -8,7 +8,7 @@ for (let i = 0; i < arr.length-2; i++) {
 	for (let j = i+1; j < arr.length-1; j++) {
 		for (let k = j+1; k < arr.length; k++) {
 			if(arr[i]+arr[j]+arr[k]>=target){
-				 ans = arr[i]+arr[j]+arr[k] - target;
+				 min = arr[i]+arr[j]+arr[k] - target;
 				if(ans<min){
 					ans = min;
 				}
