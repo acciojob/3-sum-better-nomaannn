3f4917080b22d66cfe755aeae5c91aec5d78e5cd
Nomaan Ahmad <94966523+nomaannn@users.noreply.github.com>
2023-03-28 08:01:13 +0530

    created a submission

--- script.js
+++ script.js
@@ -4,8 +4,8 @@ function threeSum(arr, target) {
 
 let ans = 0;
 
-for (let i = 0; i < arr.length; i++) {
-	for (let j = i+1; j < arr.length; j++) {
+for (let i = 0; i < arr.length-2; i++) {
+	for (let j = i+1; j < arr.length-1; j++) {
 		for (let k = j+1; k < arr.length; k++) {
 			if(arr[i]+arr[j]+arr[k]>=target){
 				 ans = arr[i]+arr[j]+arr[k] - target;
